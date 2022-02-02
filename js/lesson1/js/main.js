132bdf78239e43a31ba3141b1908daaf13da0d0d
IvanKukil <ivan.kukilAMC@gmail.com>
2022-02-02 19:37:16 +0200

    homework1 lesson1

---
+++ js/lesson1/js/main.js
@@ -0,0 +1,66 @@
+// task1
+const hi = 'hello';
+console.log(hi);
+const owu = 'owu';
+alert(owu);
+const com = 'com';
+document.write(
+    `<div>
+        ${com}
+    </div>`);
+const ua = 'ua';
+console.log(ua);
+const value1 = 1;
+console.log(value1);
+const value2 = 10;
+console.log(value2);
+const value3 = -999;
+console.log(value3);
+const value4 = 123;
+console.log(value4);
+const value5 = 3.14;
+console.log(value5);
+const value6 = 2.7;
+console.log(value6);
+const value7 = 16;
+console.log(value7);
+const value8 = true;
+console.log(value8);
+const value9 = false;
+console.log(value9);
+
+//task2
+const firstName = 'Ivan';
+const middleName = 'Dmytrovych';
+const lastName = 'Kukil';
+
+const person = firstName + ' ' + middleName + ' ' + lastName;
+
+prompt('Hello, ' + firstName + ' ' + middleName + '. How old are you?','');
+let a = 100; let b = '100'; let c = true;
+console.log(typeof (a));
+console.log(typeof (b));
+console.log(typeof (c));
+
+console.log(5 < 6);
+console.log(5 > 6);
+console.log(5 >= 6);
+console.log(5 == 6);
+console.log(10 == 10);
+console.log(10 === 10);
+console.log(10 > 10);
+console.log(10 < 10);
+console.log(10 != 10);
+console.log(123 === '123');
+console.log(123 == '123');
+
+//task3
+let str = "20";
+let x = 5;
+document.write(str + x + "<br/>");
+document.write(str - x + "<br/>");
+document.write(str * "2" + "<br/>");
+document.write(str / 2 + "<br/>");
+
+
+
